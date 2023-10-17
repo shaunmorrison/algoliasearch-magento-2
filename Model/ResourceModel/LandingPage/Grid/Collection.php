8795cbb65d51529bdc96ae261976baaf934e9731
rachel-trott <rachel.trott@algolia.com>
2023-10-17 15:44:07 -0500

    MAGE-759 fix for Method Algolia\AlgoliaSearch\Model\ResourceModel\LandingPage\Grid\Collection::setAggregations() should return $this(Algolia\AlgoliaSearch\Model\ResourceModel\LandingPage\Grid\Collection) but return statement is missing.

--- Model/ResourceModel/LandingPage/Grid/Collection.php
+++ Model/ResourceModel/LandingPage/Grid/Collection.php
@@ -5,12 +5,6 @@ namespace Algolia\AlgoliaSearch\Model\ResourceModel\LandingPage\Grid;
 use Algolia\AlgoliaSearch\Model\ResourceModel\LandingPage\Collection as LandingPageCollection;
 use Magento\Framework\Api\Search\AggregationInterface;
 use Magento\Framework\Api\Search\SearchResultInterface;
-use Magento\Framework\Api\SearchCriteriaInterface;
-use Magento\Framework\Data\Collection\Db\FetchStrategyInterface;
-use Magento\Framework\Data\Collection\EntityFactoryInterface;
-use Magento\Framework\Event\ManagerInterface;
-use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
-use Psr\Log\LoggerInterface;
 
 class Collection extends LandingPageCollection implements SearchResultInterface
 {
@@ -18,30 +12,32 @@ class Collection extends LandingPageCollection implements SearchResultInterface
     protected $aggregations;
 
     /**
-     * @param EntityFactoryInterface $entityFactory
-     * @param LoggerInterface $logger
-     * @param FetchStrategyInterface $fetchStrategy
-     * @param ManagerInterface $eventManager
-     * @param $mainTable
-     * @param $eventPrefix
-     * @param $eventObject
-     * @param $resourceModel
-     * @param $model
-     * @param $connection
-     * @param AbstractDb|null $resource
+     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface    $entityFactory
+     * @param \Psr\Log\LoggerInterface                                     $logger
+     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
+     * @param \Magento\Framework\Event\ManagerInterface                    $eventManager
+     * @param mixed|null                                                   $mainTable
+     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb         $eventPrefix
+     * @param mixed                                                        $eventObject
+     * @param mixed                                                        $resourceModel
+     * @param string                                                       $model
+     * @param null                                                         $connection
+     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb|null    $resource
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
      */
     public function __construct(
-        EntityFactoryInterface $entityFactory,
-        LoggerInterface $logger,
-        FetchStrategyInterface $fetchStrategy,
-        ManagerInterface $eventManager,
+        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
         $mainTable,
         $eventPrefix,
         $eventObject,
         $resourceModel,
         $model = 'Magento\Framework\View\Element\UiComponent\DataProvider\Document',
         $connection = null,
-        AbstractDb $resource = null
+        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
     ) {
         parent::__construct(
             $entityFactory,
@@ -72,17 +68,20 @@ class Collection extends LandingPageCollection implements SearchResultInterface
         $this->aggregations = $aggregations;
     }
 
-    /** @return SearchCriteriaInterface|null */
+    /** @return \Magento\Framework\Api\SearchCriteriaInterface|null */
     public function getSearchCriteria()
     {
         return null;
     }
 
     /**
-     * @param SearchCriteriaInterface|null $searchCriteria
-     * @return $this|Collection
+     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     *
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function setSearchCriteria(SearchCriteriaInterface $searchCriteria = null)
+    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null)
     {
         return $this;
     }
@@ -94,8 +93,11 @@ class Collection extends LandingPageCollection implements SearchResultInterface
     }
 
     /**
-     * @param $totalCount
-     * @return $this|Collection
+     * @param int $totalCount
+     *
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function setTotalCount($totalCount)
     {
@@ -103,8 +105,11 @@ class Collection extends LandingPageCollection implements SearchResultInterface
     }
 
     /**
-     * @param array|null $items
-     * @return $this|Collection
+     * @param \Magento\Framework\Api\ExtensibleDataInterface[] $items
+     *
+     * @return $this
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function setItems(array $items = null)
     {
